cb99563e651c69d572045ec45a21bdaca0fb1bb1
CodeNour <ayman.jibrael@icloud.com>
2022-02-27 15:48:07 +0300

    Initial Commit

---
+++ DarkModeMenu/DarkModeMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeMenuApp.swift
+//  DarkModeMenu
+//
+//  Created by ayhan on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
